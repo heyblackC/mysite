076a4277999e194fb626bf84721e17130be35892
chenzk <email>
2018-11-18 17:23:43 +0800

    新增了旧书籍对象，及相应的GET和POST请求

--- oldBook/views.py
+++ oldBook/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.forms import formset_factory
 import json
+# from django.core import serializers
 
 # import Model and Form
-from .models import Image
+from .models import Image, Book
 from .forms import ImageForm, BookForm
 
 
@@ -25,16 +25,23 @@ def index(request):
                         continue
                     image_instance = Image(book=book_instance, image=image_data)
                     image_instance.save()
-            return HttpResponse("ok")
+            # 新增书籍信息成功，返回书籍的id和基本信息
+            return HttpResponse(book_instance.json_data(), content_type="application/json")
         else:
-            return HttpResponse(str(bookForm.errors) + str(imageset.is_valid()))
-    elif request.method == 'PATCH':
-        return HttpResponse("qweqwewqewq")
+            # 错误处理，要返回相应的错误原因
+            error_dic = {
+                "status": "error",
+                "message": bookForm.errors,
+            }
+            return HttpResponse(json.dumps(error_dic), content_type="application/json")
+    elif request.method == 'GET':
+        book_set = Book.objects.all()
+        response_list = [book.dic_data() for book in book_set]
+        return HttpResponse(json.dumps(response_list), content_type="application/json")
+    elif request.method == 'DELETE':
+        return HttpResponse("0")
     else:
-        bookForm = BookForm()
-        imageset = ImageFormSet()
-        return render(request, 'manage_articles.html',
-                      {'postForm': bookForm, 'formset': imageset})
+        return HttpResponseNotAllowed(permitted_methods=['POST', 'GET', 'DELETE', 'PATCH'])
 
 
 
